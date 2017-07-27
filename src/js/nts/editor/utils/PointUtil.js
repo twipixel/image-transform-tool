59cd835880cee6a25145088492da48e0413003df
twipixel <twipixel@naver.com>
2017-07-27 10:44:32 +0900

    불필요한 코드 삭제

--- src/js/nts/editor/utils/PointUtil.js
+++ src/js/nts/editor/utils/PointUtil.js
@@ -12,46 +12,27 @@ export class PointUtil {
         return Math.sqrt((point1.x - point2.x) * (point1.x - point2.x) + (point1.y - point2.y) * (point1.y - point2.y));
     }
 
-    static calcDistance(point1, point2) {
-        return Math.sqrt((point1.x - point2.x) * (point1.x - point2.x) + (point1.y - point2.y) * (point1.y - point2.y));
-    }
-
-    static getAddedInterpolate(tc, bc, addLength) {
-        var h = PointUtil.calcDistance(tc, bc);
-        var f = (h + addLength) / h;
-        return PointUtil.interpolate(tc, bc, f);
+    /**
+     * interpolate 보간법에 addLength 만큼의 거리를 더한 값을 반환합니다.
+     * @param point1
+     * @param point2
+     * @param addLength
+     * @returns {PIXI.Point|*}
+     */
+    static getAddedInterpolate(point1, point2, addLength) {
+        var distance = PointUtil.distance(point1, point2);
+        var f = (distance + addLength) / distance;
+        return PointUtil.interpolate(point1, point2, f);
     }
 
     /**
-     * 지정한 두 점 사이에서 한 점을 정합니다.
-     * http://help.adobe.com/ko_KR/as2/reference/flashlite/WS5A22C182-B974-4b7e-9979-5BD0B43389F0.html
+     * factor에 의해 point1과 point2를 잇는 선분의 한점을 반환합니다.
      * @param point1
      * @param point2
-     * @param f
+     * @param f 0이면 point2값이 1이면 point1값이 나옵니다. 1.5이면 두점을 지나 1.5배 먼 거리값이 나옵니다.
      * @returns {PIXI.Point|*}
      */
     static interpolate(point1, point2, f) {
         return new PIXI.Point(point2.x + f * (point1.x - point2.x), point2.y + f * (point1.y - point2.y));
-    }
-
-    /**
-     * (0,0)과 현재 포인트 사이의 선분을 설정된 길이로 조절합니다.
-     * http://help.adobe.com/ko_KR/as2/reference/flashlite/WSB22C5AE6-750E-4274-BBF4-C10BD879207C.html
-     * @param point
-     * @param length
-     * @returns {*}
-     */
-    static normalize(point, length) {
-        if (point.x == 0 && point.y == 0)
-            return point;
-
-        var norm = length / Math.sqrt(point.x * point.x + point.y * point.y);
-        point.x *= norm;
-        point.y *= norm;
-        return point;
-    }
-
-    constructor() {
-
     }
 }
